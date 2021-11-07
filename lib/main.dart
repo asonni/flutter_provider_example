82c5f5c420329946bd8bf95217ccc2cf4abf7c21
Aladdin Sonni <aladdin.sonni@gmail.com>
2021-11-07 15:16:28 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<Data>(
+      create: (context) => Data(),
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const MyText(),
+          ),
+          body: const Level1(),
+        ),
+      ),
+    );
+  }
+}
+
+class Level1 extends StatelessWidget {
+  const Level1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Level2();
+  }
+}
+
+class Level2 extends StatelessWidget {
+  const Level2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        MyTextField(),
+        Level3(),
+      ],
+    );
+  }
+}
+
+class Level3 extends StatelessWidget {
+  const Level3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(Provider.of<Data>(context).data);
+  }
+}
+
+class MyText extends StatelessWidget {
+  const MyText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(Provider.of<Data>(context).data);
+  }
+}
+
+class MyTextField extends StatelessWidget {
+  const MyTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: (newText) {
+        // context.read<Data>().changeString(newText)
+        Provider.of<Data>(context, listen: false).changeString(newText);
+      },
+    );
+  }
+}
+
+class Data extends ChangeNotifier {
+  String data = 'Some data';
+
+  void changeString(String newString) {
+    data = newString;
+    notifyListeners();
+  }
+}
